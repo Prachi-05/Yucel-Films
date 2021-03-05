e0d4f37585e4bf049f5796223330a369ab72c13e
prachisabarad <prachi.sabarad@gmail.com>
2021-03-05 21:31:30 +0530

    Initial

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<?php 
+    include("config.php"); 
+?>
+<html>
+<head>
+	<title>Yucel Films</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    
+	<script src="script.js"></script>
+</head>
+
+<body>
+    <header class="menu">
+        <div class="nav-header">
+            <div class="nav-logo">
+                <a href="Index.php"><img src="assets/icon/YucelFilms_Logo.svg" class="nav-logo"></a>
+            </div>
+            <ul>
+                <li><a href="worksdemo2.php" id="works">WORKS</a></li>            
+                <li><a href="About.php" id="about">ABOUT US </a></li>                
+                <li><a href="Contacts.php" id="contacts">CONTACTS </a></li>
+            </ul>
+        </div>
+    </header>
+
+ <div id="mainContent">
