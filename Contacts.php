1635a49bf985fbb558c7233c3069aa92a292d1da
prachisabarad <prachi.sabarad@gmail.com>
2021-03-05 21:41:39 +0530

    Initial

---
+++ Contacts.php
@@ -0,0 +1,40 @@
+<?php
+include("includes/header.php"); 
+include("includes/footer.php"); 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Yucel Films</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="../carousel.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+	<div class="contact">
+    	<div class="email"><a href="mailto:yucelfilms@gmail.com">yucelfilms@gmail.com</a></div>
+    	<div class="phone"><a href="tel:+91 8281769078"><img src="./assets/icon/PhoneNumber.svg" class="phone"> </a></div>
+    	
+	</div>
+
+
+<!------------- JAVASCRIPT BEGINS HERE --------------->
+	<script type="text/javascript">
+
+		var works = document.getElementById("works");
+		works.setAttribute('style', "color: black"); 
+
+		var about = document.getElementById("about");
+		about.setAttribute('style', "color: black");
+
+		var contacts=document.getElementById("contacts");
+		contacts.setAttribute('style', "color: #ff6238"); 
+	</script>
+</body>
+</html>
